42ed4f55e0a643fb81f7870f7174a33817378f9a
satya405 <41770337+satya405@users.noreply.github.com>
2018-07-27 16:07:23 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,7 +1,8 @@
-b=4
-if(b % 2 ==0):
-    print('even')
-elif(b % 2 <>0):
-    print('odd')
+b=int(input())
+if(b>0)
+   if(b%2==0):
+       print('even')
+   else(b%2<>0):
+       print('odd')
 else:
     print('invalid')
