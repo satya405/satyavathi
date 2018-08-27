d8e148ed2f77ac68e38e690bbf5a81e6df03c418
satya405 <41770337+satya405@users.noreply.github.com>
2018-08-27 13:12:13 +0530

    Create three.py

---
+++ three.py
@@ -0,0 +1,13 @@
+u=int(input('enter a num'))
+o=int(input('enter a num'))
+p=int(input('enter a num'))
+if(u>o):
+     if(u>p):
+         print('u is greater')
+     else:
+         print('p is greater')
+else:
+       if(o>p):
+          print('o is greater')
+       else:
+          print('p is greater')
