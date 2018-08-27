114f5980bb2e8cf4a06d50762440f80bbf701313
satya405 <41770337+satya405@users.noreply.github.com>
2018-08-27 12:54:16 +0530

    Create vow.py

---
+++ vow.py
@@ -0,0 +1,9 @@
+w=raw_input('')
+vowel=['a','e','i','o','u']
+if(i>='a' and i<='z'):
+     if(i in vowel):
+         print('Vowel')
+     else:
+         print('Consonant')
+else:
+    print('invalid')
