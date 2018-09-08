ccf69bd80c8f9fdb28037523a32a39ff194c02f6
satya405 <41770337+satya405@users.noreply.github.com>
2018-09-08 09:29:20 +0530

    Create arm.py

---
+++ arm.py
@@ -0,0 +1,12 @@
+n = int(input())
+order = len(str(num))
+sum = 0
+temp = n
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** order
+   temp //= 10
+if n == sum:
+   print("yes")
+else:
+   print("no")
